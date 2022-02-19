83dfffbb4ca857756dae5cc9919a6e9075f63ce6
RobertaRaj <92271727+RobertaRaj@users.noreply.github.com>
2022-02-19 11:43:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,180 @@
+//step01
+// let someMonth;          
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction
+
+
+// numeric literal expression  = 216
+
+// var yearsRunning = 6;
+// window.console.log("Roberta has been running for " + yearsRunning + " years.");
+
+
+
+//string literal expression  ='anything'
+
+// let name = "Roberta"
+// alert (`This is my string literal expression ${name}`)
+
+
+//Boolean literal expression = true/false
+
+// var yearsRunning = "true";
+// yearsRunning = true;
+// window.console.log(yearsRunning);
+
+//Null literal expression  = null
+
+// var lastName = null;
+// window.console.log(lastName);
+
+
+//Complex/variable expressions
+// let anExpression = 2 * (10/5)  +20;
+// window.console.log(anExpression);
+//  let anotherExpression= 10 * (40/20) - 11;
+//  window.console.log(anotherExpression);
+
+
+// Declare 9 variables:
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+
+
+//step05
+// let firstName;
+// firstName='Roberta';
+
+// let address;
+// address='via santa monica n.1';
+
+// let city;
+// city='San Diego';
+// window.console.log(firstName, address, city);
+
+
+//step06 Add a number and a string 
+// let yearsRunning= 6;
+// alert ('Roberta has been running for '+ yearsRunning +' years.');
+// windows.console.log(yearsRunning);
+
+//step07 add boolean and string
+// let yearsRunning= true;
+// alert ('Roberta has been running for '+ yearsRunning +' years.');
+// window.console.log(yearsRunning);
+
+//step07 , add a number and a Boolean 
+// let yearsRunning= 6;
+// alert ('Roberta has been running for '+ false +' years.');
+// window.console.log(yearsRunning);
+
+
+//step08 it is valid string.
+
+
+
+// step 9.	Create a variable that produces a null value in the console window. Then, create a variable that produces an undefined value in the console window
+// let firstName= 'Null'; 
+// window.console.log(firstName);
+
+// let firstName;
+// alert (firstName);
+
+
+
+//step 10.	Use the unary type of operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+// var num = "Roberta";
+// window.console.log(typeof num);
+ 
+// var num = 26;
+// window.console.log(typeof num);
+
+// var num = new Object();
+// window.console.log(typeof num);
+
+// var num = true;
+// window.console.log(typeof num);
+
+// var num = undefined;
+// window.console.log(typeof num);
+
+//step 11  use the concatenation operator (+) 
+// var name = 'Roberta Cavallaro';
+// var name2= 'Javascript';
+// alert('Hello ' + name + ', welcome to the ' + name2 + ' class');
+
+
+// alert ("Hello Roberta Cavallaro "  +  ", Welcome to the Javascript class!");
+// window.console.log(alert);
+
+//step 12.	Declare a variable called name and set it equal to your name. Substitute your name in the previous alert string with the variable instead.
+// var name = 'Roberta';
+
+//step13
+// var course = 'Javascript';
+
+
+//step14
+//  var name = 'Roberta Cavallaro';
+//  var name2= 'Javascript';
+//  alert('Hello ' + name + '.\nWelcome to the ' + name2 + ' class');
+
+
+//step 15.	Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable
+// let name = prompt ('Insert your first name');
+// window.console.log(name);
+
+
+//step16
+// let classes = prompt('Insert which class you are in');
+// window.console.log(classes);
+
+//step17 
+// let x = 10;
+// let y = 20;
+// let sum = x + y;
+// window.console.log(x+y);
+
+
+// step18
+// var x = 20
+// x += 20;
+// window.console.log(x);
+
+
+// step19
+// var x= 20;
+// x *= 5;
+// window.console.log(x);
+
+
+// step20
+// var x= 20 % 3;
+// window.console.log(x);
+
+
+// step21.	Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result within the console window.
+// let x = 10;
+// let y = 20;
+// alert (x < y);
+
+
+// step22
+let x = 300
+let y = 100
+alert (x  <= y);
+
+
+
+
+
